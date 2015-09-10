9167ae5c8e575ff744508d31e970e38f2913b610
RLund16 <RLund16@S-LIBRARY17.jesuitpdx.org>
2015-09-10 12:25:51 -0700

    improved fighter class formatting

--- src/classes/monk/Monk.java
+++ src/classes/monk/Monk.java
@@ -1,13 +1,22 @@
 package classes.monk;
 
+import actions.Action;
+import static actions.Action.Type.BONUS_ACTION;
 import amounts.AddedAmount;
 import amounts.ConditionalAmount;
 import amounts.Value;
 import classes.PlayerClass;
 import enums.AbilityScore;
-import static enums.AbilityScore.*;
+import static enums.AbilityScore.DEX;
+import static enums.AbilityScore.STR;
+import static enums.AbilityScore.WIS;
 import enums.Skill;
-import static enums.Skill.*;
+import static enums.Skill.Acrobatics;
+import static enums.Skill.Athletics;
+import static enums.Skill.History;
+import static enums.Skill.Insight;
+import static enums.Skill.Religion;
+import static enums.Skill.Stealth;
 import player.KiComponent;
 import player.Player;
 
@@ -48,5 +57,40 @@ public class Monk extends PlayerClass {
     @Override
     public Skill[] skills() {
         return new Skill[]{Acrobatics, Athletics, History, Insight, Religion, Stealth};
+    }
+
+    public class FlurryOfBlows extends Action {
+
+        public FlurryOfBlows(Creature creature) {
+
+        }
+
+        @Override
+        protected void act() {
+
+        }
+
+        @Override
+        public String[] defaultTabs() {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public Type getType() {
+            return BONUS_ACTION;
+        }
+
+        public String getName() {
+            return "Flurry of Blows";
+        }
+
+        @Override
+        public String getDescription() {
+            return "Immediately after you take the Attack action on your turn, you can spend 1 ki point to make two unarmed strikes as a bonus action";
+        }
+
+        public boolean isAvailable() {
+
+        }
     }
 }
