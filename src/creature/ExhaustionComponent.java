c9458ad8eafe7de473e951223a8cffdcf82dc978
RLund16 <RLund16@S-LIBRARY17.jesuitpdx.org>
2015-09-24 12:31:42 -0700

    Exhaustion Class

--- src/creature/ExhaustionComponent.java
+++ src/creature/ExhaustionComponent.java
@@ -6,9 +6,7 @@
 package creature;
 
 import core.AbstractComponent;
-import events.AbilityCheckEvent;
-import events.EventListener;
-import events.TurnStartEvent;
+import events.*;
 
 /**
  *
@@ -23,15 +21,53 @@ public class ExhaustionComponent extends AbstractComponent {
         this.creature = creature;
         EventListener.createListener(creature, AbilityCheckEvent.class, e -> {
             if (e.creature == creature) {
-                if (e.creature.) {
+                if (e.creature.exc.exhaustionLevel >= 1) {
                     e.advantage = false;
+                }
+            }
+
+        });
+
+        EventListener.createListener(creature, TurnStartEvent.class, e -> {
+            if (e.creature == creature) {
+                if (e.creature.exc.exhaustionLevel >= 2) {
+                    e.creature.spc.landSpeed.multComponents.put("ExhaustionSpeed", .5);
                 }
             }
         });
 
-        EventListener.createListener(creature, TurnStartEvent.class, e -> {
-
+        EventListener.createListener(creature, SavingThrowEvent.class, e -> {
+            if (e.creature == creature) {
+                if (e.creature.exc.exhaustionLevel >= 3) {
+                    e.disadvantage = true;
+                }
+            }
         });
+        /**
+         * EventListener.createListener(creature, AttackRollEvent.class, e->{
+         * if(e.creature==creature) if(e.creature.exc.exhaustionLevel>=3)
+         * e.disadvantage = true; });
+        *
+         */
+        EventListener.createListener(creature, TurnStartEvent.class, e -> {
+            if (e.creature == creature) {
+                if (e.creature.exc.exhaustionLevel >= 4) {
+                    e.creature.hc.maxHealth
+
+                }
+            }
+        });
+
+        /**
+         * 6 levels of exhaustion Exhaustion levels stack Have functions that
+         * add and subtract exhaustion (for exhaustion and rest) Each creature
+         * has its own level of exhaustion Not necessary to know the source of
+         * the exhaustion Exhaustion will be a component not a condition          *
+         * 4th level of exhaustion should be a conditional amount. It will be
+         * added onto health in all situations.
+         *
+         *
+         */
     }
 
     public void addExhaustion() {
