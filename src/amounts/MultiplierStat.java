2d77f9a51e03acf720cf0a28cb8055145e711466
RLund16 <RLund16@S-LIBRARY17.jesuitpdx.org>
2015-09-28 11:59:33 -0700

    Exhaustion Class & Double Amount

--- src/amounts/MultiplierStat.java
+++ src/amounts/MultiplierStat.java
@@ -5,16 +5,16 @@ import java.util.HashMap;
 public class MultiplierStat implements Amount {
 
     public final HashMap<Object, Amount> flatComponents = new HashMap();
-    public final HashMap<Object, Double> multComponents = new HashMap();
+    public final HashMap<Object, DoubleAmount> multComponents = new HashMap();
 
     @Override
     public int get() {
-        return (int) (flatComponents.values().stream().mapToInt(Amount::get).sum() * multComponents.values().stream().reduce(1., (a, b) -> a * b));
+        return (int) (flatComponents.values().stream().mapToInt(Amount::get).sum() * multComponents.values().stream().mapToDouble(DoubleAmount::get).reduce(1., (a, b) -> a * b));
     }
 
     @Override
     public int roll() {
-        return (int) (flatComponents.values().stream().mapToInt(Amount::roll).sum() * multComponents.values().stream().reduce(1., (a, b) -> a * b));
+        return (int) (flatComponents.values().stream().mapToInt(Amount::roll).sum() * multComponents.values().stream().mapToDouble(DoubleAmount::get).reduce(1., (a, b) -> a * b));
     }
 
     public void set(int amt) {
